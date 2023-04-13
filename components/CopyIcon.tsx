2e67a84891f146580b326e1c4ef0c5a2130ee51c
Luis Egea <alphasearchseo@gmail.com>
2023-04-13 15:31:36 -0500

    chore: add copy address icon & logic

---
+++ components/CopyIcon.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+type CopyIconProps = {
+  textToCopy: string;
+};
+
+export const CopyIcon: React.FC<CopyIconProps> = ({ textToCopy }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyText = async () => {
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 1500);
+    } catch (err) {
+      console.error('Failed to copy text: ', err);
+    }
+  };
+
+  return (
+    <div className="cursor-pointer w-6" onClick={copyText}>
+      {isCopied ? <span className='ml-3 text-green-500'> Copied!</span> : <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path d="M7 3.5A1.5 1.5 0 018.5 2h3.879a1.5 1.5 0 011.06.44l3.122 3.12A1.5 1.5 0 0117 6.622V12.5a1.5 1.5 0 01-1.5 1.5h-1v-3.379a3 3 0 00-.879-2.121L10.5 5.379A3 3 0 008.379 4.5H7v-1z"></path>
+        <path d="M4.5 6A1.5 1.5 0 003 7.5v9A1.5 1.5 0 004.5 18h7a1.5 1.5 0 001.5-1.5v-5.879a1.5 1.5 0 00-.44-1.06L9.44 6.439A1.5 1.5 0 008.378 6H4.5z"></path>
+      </svg>}
+    </div>
+  );
+};
--- components/nftCard.tsx
+++ components/nftCard.tsx
@@ -1,3 +1,5 @@
+import { CopyIcon } from './CopyIcon';
+
 interface Media {
   gateway: string;
 }
@@ -18,27 +20,29 @@ interface NFT {
   description: string;
 }
 
-export const NFTCard = ({ nft }: {nft: NFT}) => {
+export const NFTCard = ({ nft }: { nft: NFT }) => {
 
-  console.log("nft:", nft)
+  //console.log("nft:", nft)
 
   return (
-      <div className="w-1/4 flex flex-col ">
+    <div className="w-1/4 flex flex-col ">
       <div className="rounded-md">
-          <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+        <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
       </div>
       <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
-          <div className="">
-              <h2 className="text-xl text-gray-800">{nft.title}</h2>
-              <p className="text-gray-600">Id: {nft.id.tokenId}</p>
-              <p className="text-gray-600" >{nft.contract.address}</p>
-          </div>
-
-          <div className="flex-grow mt-2">
-              <p className="text-gray-600">{nft.description}</p>
-          </div>
+        <div>
+          <h2 className="text-xl text-gray-800">{nft.title}</h2>
+          <p className="text-gray-600">Id: {parseInt(nft.id.tokenId)}</p>
+          <p className="flex text-gray-600" >
+            {(nft.contract.address).slice(0, 5)}...{(nft.contract.address).slice(-5)}
+            <CopyIcon textToCopy={nft.contract.address} />
+          </p>
+        </div>
+        <div className="flex-grow mt-2">
+          <p className="text-gray-600">{nft.description}</p>
+        </div>
       </div>
 
-  </div>
+    </div>
   )
 }
